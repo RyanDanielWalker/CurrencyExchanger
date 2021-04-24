b59a34bfc75716b90252474b843be2ce83e4eca8
Ryan Walker <ryandanielwalker@gmail.com>
2021-04-23 18:10:55 -0700

    Remove console.log and remove API variable

--- src/js/exchanger-service.js
+++ src/js/exchanger-service.js
@@ -1,9 +1,7 @@
 export default class Exchanger {
   static exchangeCurrency(exchangeTo, userAmount) {
-    const apiKey = process.env.API_KEY;
-    return fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/pair/USD/${exchangeTo}/${userAmount}`, { mode: "cors" })
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${exchangeTo}/${userAmount}`, { mode: "cors" })
       .then(function (response) {
-        console.log(response);
         if (!response.ok) {
           throw Error(response.statusText);
         }
